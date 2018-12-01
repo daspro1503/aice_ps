4fe8a6c18a0d55a750cbc3860078b8e3258955c7
daspro1503 <jembadavid45@gmail.com>
2018-12-02 02:04:53 +0300

    Update views.py

--- api/v1/views.py
+++ api/v1/views.py
@@ -7,28 +7,28 @@ test=models.ireporter()
 def sign_up():
     request_data=request.get_json()
     if not request.json :
-        return "fasd"
+        return "Enter parameters in acceptable json format and try again",403
     else:
         reply=test.signup(request_data['firstname'],request_data['lastname'],request_data['othername'],request_data['email'],request_data['phonenumber'],request_data['username'],request_data['password'])       
-        response=Response("d",201,mimetype="application/json")
+        
         return jsonify(reply)
 
 @app.route('/api/v1/red-flag', methods = ['POST'])
 def create_red_flag():
     request_data=request.get_json()
     if not request.json :
-        return "fasd"
+        return "Enter parameters in acceptable json format and try again",403
     else:
         reply=test.create_incident(request_data['CreatedBy'],request_data['i_type'],request_data['location'],request_data['images'],request_data['videos'],request_data['comment'])       
         
-        #response=Response("d",201,mimetype="application/json")
+        
         return jsonify({'data':reply})    
 
 @app.route('/api/v1/all_red-flags', methods = ['GET'])
 def get_all_redflags():
     request_data=request.get_json()
     if not request.json :
-        return "fasd"
+        return "Enter parameters in acceptable json format and try again",403
     else:
         reply=test.get_all(request_data['CreatedBy'])       
         
@@ -40,7 +40,7 @@ def edit_location(id):
     
     request_data=request.get_json()
     if not request.json :
-        return "fasd"
+        return "Enter parameters in acceptable json format and try again",403
     else:
         reply=test.patch_record(id,"location",request_data['value'])       
         
@@ -51,29 +51,29 @@ def edit_comment(id):
     
     request_data=request.get_json()
     if not request.json :
-        return "fasd"
+        return "Enter parameters in acceptable json format and try again",403
     else:
         reply=test.patch_record(id,"comment",request_data['value'])       
         
-        #response=Response("d",201,mimetype="application/json")
+       
         return jsonify({'data':reply})  
 @app.route('/api/v1/edit_images/<int:id>', methods = ['PATCH'])
 def edit_images(id):
     
     request_data=request.get_json()
     if not request.json :
-        return "fasd"
+        return "Enter parameters in acceptable json format and try again",403
     else:
         reply=test.patch_record(id,"images",request_data['value'])       
         
-        #response=Response("d",201,mimetype="application/json")
+        
         return jsonify({'data':reply})
 @app.route('/api/v1/edit_videos/<int:id>', methods = ['PATCH'])
 def edit_videos(id):
     
     request_data=request.get_json()
     if not request.json :
-        return "fasd"
+        return "Enter parameters in acceptable json format and try again",403
     else:
         reply=test.patch_record(id,"videos",request_data['value'])       
         
@@ -84,7 +84,7 @@ def delete_record(id):
     
     request_data=request.get_json()
     if not request.json :
-        return "fasd"
+        return "Enter parameters in acceptable json format and try again",403
     else:
         reply=test.delete_record(id)       
         
